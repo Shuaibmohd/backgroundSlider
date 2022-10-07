908fb47b7330f3750a13b3dca23e05cad54e0eeb
sheebukhan62 <sheebukhan62@gmail.com>
2022-10-07 23:22:47 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide');
+const rightBtn = document.getElementById('right');
+const leftBtn = document.getElementById('left');
+
+let activeSlide = 0
+
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+    
+    if(activeSlide > slides.length - 1){
+        activeSlide = 0
+    }
+
+    setBGtoBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+
+    if(activeSlide < 0){
+        activeSlide = slides.length - 1
+    }
+
+    setBGtoBody()
+    setActiveSlide()
+})
+
+
+
+setBGtoBody()
+
+
+
+function setBGtoBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
